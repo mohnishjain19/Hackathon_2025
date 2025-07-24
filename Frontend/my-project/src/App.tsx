e3caab0f3b407614ab4ee8d8edf2c5cb353a5b5f
mohnishjain19 <mohnishjain29@gmail.com>
2025-07-24 13:46:01 +0530

    Frontend

--- Frontend/my-project/src/App.tsx
+++ Frontend/my-project/src/App.tsx
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom"
 import { Form } from "./Components/Form"
 import { HelpDesk } from "./Components/HelpDesk"
 import { Home } from "./Components/Home"
+import { Layout } from "./Components/Layout/layout"
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/help-desk" element={<HelpDesk />}></Route>
-      <Route path="/form" element={<Form />}></Route>
+    <Routes >
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/help-desk" element={<HelpDesk />}></Route>
+        <Route path="/form" element={<Form />}></Route>
+      </Route>
     </Routes>
     </>
   )
